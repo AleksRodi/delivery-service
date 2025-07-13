17ff4118beaf440aaa2ad89c28d1b2703f033da9
AleksRodi <rodionov_job@mail.ru>
2025-07-13 15:41:05 +0300

    Create axios.js

---
+++ frontend/src/axios.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const instance = axios.create({
+  baseURL: "http://localhost:8000", // или ваш продовый адрес
+});
+
+instance.interceptors.request.use(config => {
+  const token = localStorage.getItem("access_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+export default instance;
