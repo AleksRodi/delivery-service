4e1bb6878612d1f79c340be6968e3163bbdb6cc7
AleksRodi <rodionov_job@mail.ru>
2025-07-13 15:18:12 +0300

    Update __init__.py

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,3 +1,8 @@
 from .user import router as user_router
 from .bot import router as bot_router
 from .category import router as category_router
+from .product import router as product_router
+from .customer import router as customer_router
+from .order import router as order_router
+from .payment_settings import router as payment_settings_router
+from .delivery_settings import router as delivery_settings_router
