bdf12a8065e83d52e70d15a98296879ff5b8e418
AleksRodi <rodionov_job@mail.ru>
2025-07-13 16:09:15 +0300

    Update axios.js

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000", // или ваш продовый адрес
+  baseURL: process.env.REACT_APP_API_URL || "http://localhost:8000",
 });
 
 instance.interceptors.request.use(config => {
