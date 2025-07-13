4b1a676fe8f333a836d4c7da7f7f7dac174bb1cb
AleksRodi <rodionov_job@mail.ru>
2025-07-13 15:18:31 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
-from app.api import user_router, bot_router, category_router
+from app.api import (
+    user_router,
+    bot_router,
+    category_router,
+    product_router,
+    customer_router,
+    order_router,
+    payment_settings_router,
+    delivery_settings_router,
+)
 
 app = FastAPI()
 
 app.include_router(user_router)
 app.include_router(bot_router)
 app.include_router(category_router)
+app.include_router(product_router)
+app.include_router(customer_router)
+app.include_router(order_router)
+app.include_router(payment_settings_router)
+app.include_router(delivery_settings_router)
 
 @app.get("/")
 def read_root():
