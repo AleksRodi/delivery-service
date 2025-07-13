041a01fedaabe63288597b49566b0fa608d68a4d
AleksRodi <rodionov_job@mail.ru>
2025-07-13 15:24:16 +0300

    Update bot.py

--- backend/app/crud/bot.py
+++ backend/app/crud/bot.py
@@ -3,6 +3,7 @@ from sqlalchemy.future import select
 from sqlalchemy.exc import IntegrityError
 from app.db.models.bot import Bot
 from app.schemas.bot import BotCreate
+from app.services.bot_webhook import set_webhook
 
 async def get_bot_by_id(db: AsyncSession, bot_id: int):
     result = await db.execute(select(Bot).where(Bot.id == bot_id))
@@ -38,3 +39,23 @@ async def delete_bot(db: AsyncSession, bot_id: int):
     await db.delete(bot)
     await db.commit()
     return bot
+
+async def create_bot(db: AsyncSession, user_id: int, bot: BotCreate, base_webhook_url: str):
+    db_bot = Bot(
+        user_id=user_id,
+        name=bot.name,
+        telegram_token=bot.telegram_token,
+        webhook_url=None,
+        status="inactive"
+    )
+    db.add(db_bot)
+    try:
+        await db.commit()
+        await db.refresh(db_bot)
+        await set_webhook(db_bot, base_webhook_url)
+        await db.commit()
+        await db.refresh(db_bot)
+        return db_bot
+    except IntegrityError:
+        await db.rollback()
+        return None
