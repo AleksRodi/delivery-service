a4df27c6c4649d10275753f43e6bbb2fc34c9d8b
AleksRodi <rodionov_job@mail.ru>
2025-07-13 14:48:09 +0300

    Update session.py

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://user:password@localhost:5432/db_name")
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
